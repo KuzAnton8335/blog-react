f922794cc3f1bcdefc19722b152ee0ac675847ac
KuzAnton8335 <ant2783@gmail.com>
2025-03-22 17:27:38 +0300

    реализован основоной компонент с роутингом

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import 'normalize.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import Blog from './blog.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<Blog />
+		</BrowserRouter>
 	</StrictMode>,
 );
